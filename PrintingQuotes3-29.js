8b51fd8c078c34edca400d8e19e0630d8dc5c66b
Laimonas <ldirgelas530@gmail.com>
2024-08-01 12:41:50 +0300

    Initial commit

---
+++ PrintingQuotes3-29.js
@@ -0,0 +1,19 @@
+// Example Output
+
+//What is the quote? There aren't the droid you're looking for.
+//Who said it? Obi-Wan Kenobi
+//Obi-Wan Kenobi says "These aren't the droids
+//you're looking for."
+
+//Use string concatenation
+// Dont use string interpolation
+
+
+
+text1 = "What is the quote? There aren't the droid you're looking for. \n";
+text2 = "Who said it? Obi-Wan Kenobi\n";
+text3 = "Obi-Wan Kenobi says \"These aren't the droids\n";
+text4 = "you're looking for.\"";
+let result;
+
+console.log(result =text1.concat(text2,text3,text4));
